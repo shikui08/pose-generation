41e218173031f4b584d70cb8b856c8f0cfa5dc7e
bronwynbiro <bronwyn.biro@gmail.com>
2021-02-01 13:51:35 -0700

    added basic movement

--- mocap.py
+++ mocap.py
@@ -1,59 +1,36 @@
 import bpy
 import csv
 
-'''
-obj = bpy.context.active_object
-ol = obj.location
-
-# set current frame to 1
-#bpy.ops.rigidbody.bake_to_keyframes()
-bpy.context.scene.frame_set(1)
-
-pos_1 = (ol.x, ol.y, ol.z)
-
-# set current frame to 5
-bpy.context.scene.frame_set(5)
-pos_5 = (ol.x, ol.y, ol.z)
-
-# set current frame to 10
-bpy.context.scene.frame_set(10)
-pos_10 = (ol.x, ol.y, ol.z)
-
-# set current frame back to 1
-bpy.context.scene.frame_set(1)
-
-print('Frame 1 {0}, id: {1}'.format(pos_1, id(pos_1)))
-print('Frame 5 {0}, id: {1}'.format(pos_5, id(pos_5)))
-print('Frame 10 {0}, id: {1}'.format(pos_10, id(pos_10)))
-
-for collection in bpy.data.collections:
-   print(collection.name)
-   for obj in collection.all_objects:
-      print("obj: ", obj.name)
-      
-'''
-
 fp = "../../../..//Users/bronwynbiro/Documents/school/cmpt/415/v1_translation.csv"
 #collection = bpy.data.collections['person']
-for collection in bpy.data.collections:
-   print(collection.name)
-   for i, obj in enumerate(collection.all_objects):
-       name = obj.name
-       if name[0] == "o":
-           obj.location.x = 0 + i
-           obj.location.y = 0
-           obj.location.z = 0
-           print(obj.location)
-    
-
-'''
 
 with open(fp) as f:
     rdr = csv.reader(f)
-    for i, row in enumerate(rdr):
-        time, x, y, z = row
-        print(x, y, x)
-
-        # Generate UV sphere at x = lon and y = lat (and z = 0 )
-        bpy.ops.mesh.primitive_uv_sphere_add(location = (x, y, z))
-'''
+    file_lst = list(rdr)
+    n = len(file_lst)
+    
+    people = []
+    
+    for collection in bpy.data.collections:
+       for i, obj in enumerate(collection.all_objects):
+           name = obj.name
+           if name[0] == "o":
+               people.append(obj)
+    
+    
+    boundary = n // len(people)
+    print(n, len(people), boundary)
+    # 0, 342, go by increments of 114
+    # 342, 
+    #25330 74 342
+    for i, obj in enumerate(people):
+        start = i * boundary
+        for j in range(start, start + boundary, boundary // 3):
+            print(j)
+            time, x, y, z = file_lst[j]
+            obj.location.x = float(x)
+            obj.location.y = float(y)
+            obj.location.z = 1.7581431008140374
+            #obj.location.z = float(z)
+            #print(obj.location)
+        
