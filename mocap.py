bcd3317923a26c93e0a724cafb2bdffcec80661b
bronwynbiro <bronwyn.biro@gmail.com>
2021-02-08 17:04:40 -0700

    rewriting to use frames

--- mocap.py
+++ mocap.py
@@ -1,74 +1,75 @@
 import bpy
 import csv
 from mathutils import Euler
+from collections import defaultdict
 
 translation = "../../../..//Users/bronwynbiro/Documents/school/cmpt/415/v1_translation.csv"
 orientation = "../../../..//Users/bronwynbiro/Documents/school/cmpt/415/v1_orientation.csv"
+timestamps = "../../../..//Users/bronwynbiro/Documents/school/cmpt/415/timestamps.csv"
 collection = bpy.data.collections['Collection']
 
-def set_positions(people):
-    start, end = people[0][0], people[-1][0]
-    n = len(o_lst)
-    boundary =  n // len(people)
-    
-    # Set the orientation and rotation per person
-    for i, obj_tuple in enumerate(people):
-        frame, obj = obj_tuple
+def get_timestamps():
+    time_to_frame = defaultdict(int)
+    with open(timestamps) as f:
+        for i, line in enumerate(f):
+            _, time = line.split(",")
+            time = round(float(time), 2)
+            time_to_frame[time] = i
+    return time_to_frame
+  
+def set_position(people, times):
+    #time_to_data = defaultdict(list)
+    set_frames = set()
+    with open(translation) as t:
+        with open(orientation) as o:
+            for trans_line, o_line in zip(t, o):
+                time, x, y, z = [float(x) for x in trans_line.split(",")]
+                time_o, roll, pitch, yaw = [float(x) for x in o_line.split(",")]
+                
+                time = round(float(time), 2)
+                
+                if time in times:
+                    frame = times[time]
+                    if frame in people and frame not in set_frames:
+                        print(frame)
+                        obj = people[frame][0]
+                        
+                        #obj.hide_set(False)
+                        #obj.hide_viewport = False
+                        #obj.hide_render = False
+                        # Show and set object
+                        obj.hide_render = False
+                        #obj.keyframe_insert(data_path="hide_render", frame=frame)
+                        obj.location = (x, float(y) * -1, 0) 
+                        obj.rotation_euler =  Euler((roll + 1.75814, pitch, 0), 'XYZ')
+                        bpy.context.scene.frame_set(frame)
+                        
+                        o#bj.hide_set(True)
+                        #obj.hide_viewport = True
+                        obj.hide_render = True
+                        #obj.keyframe_insert(data_path="hide_render", frame=frame+1)
+                        #bpy.context.scene.frame_set(frame+1)
+                        set_frames.add(frame)
+                        
         
-        #bpy.context.scene.frame_set(frame)
-        
-        frame = int(frame)
-        
-        bpy.context.scene.frame_set(frame)
-        
-        #start = frame *  i#i * boundary
-        #for j in range(start, end, boundary): 
-     
-        for j in range(start, start + boundary, boundary // 3):
-            time, x, y, z = t_lst[j]
-            time_o, roll, pitch, yaw = o_lst[j]
-            obj.location.x = float(x)
-            obj.location.y = float(y) * -1
-            obj.location.z = 0 #float(z)
-            obj.rotation_euler =  Euler((float(roll) + 1.75814, float(pitch), 0), 'XYZ')
-            obj.hide_set(False)
-            bpy.context.scene.frame_set(j)
-        
-            
-        #obj.hide_set(True)
-        #bpy.context.scene.frame_set(start+boundary+1)
-        #obj.hide_render = True
-            #obj.rotation_euler =  Euler((float(roll) + 1.75814, float(pitch), 0), 'XYZ')
-            #obj.rotation_euler =  Euler((float(roll) + 1.75814, 0, 0), 'XYZ') #float(pitch), 0), 'XYZ')
-        #print(obj.rotation_euler, obj.animation_data)
-        
-with open(translation) as t, open(orientation) as o:
-    # Read in translation
-    t_rdr = csv.reader(t)
-    t_lst = list(t_rdr)
-
-    # Read in orientation
-    o_rdr = csv.reader(o)
-    o_lst = list(o_rdr)
-
+def get_meshes():
     # Get all the people meshes
-    people = []
-    seen = set()
-    for x in collection.all_objects:
-        for i, obj in enumerate(collection.all_objects): 
-           name = obj.name
-           # obj.hide_set(True)
-           if name[0] == "o" and "empty" not in name and name not in seen:
-               seen.add(name)
-               frame = obj.name.split("o_")[-1]
-               if frame.isdigit():
-                   people.append((int(frame), obj))
-    
-                   
-    people.sort(key=lambda obj: obj[0])
-    print(len(people))
-    print(len(o_lst)) # 25330 
-    n = len(o_lst)
-    boundary =  n // len(people)
-    print(boundary)
-    set_positions(people)
+    people = defaultdict(list)
+    for obj in collection.all_objects:
+        name = obj.name
+        if name[0] == "o" and "empty" not in name: 
+            frame = obj.name.split("o_")[-1]
+            if frame.isdigit():
+               people[int(frame)] = [obj]
+        
+    return people
+  
+def main():
+    times = get_timestamps()  
+    #print(times)    
+    people = get_meshes()
+    #print(people)
+    set_position(people, times)
+
+main()
+
