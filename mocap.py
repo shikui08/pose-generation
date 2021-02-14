b7b103ef0711e321acc07a2e50617772138d9475
bronwynbiro <bronwyn.biro@gmail.com>
2021-02-13 22:13:37 -0700

    resolve multiple mesh bug

--- mocap.py
+++ mocap.py
@@ -20,16 +20,14 @@ def get_timestamps():
 def get_position(people, times):
     frame_to_data = defaultdict(list)
     set_frames = set()
-    match = 0
     with open(translation) as t:
         with open(orientation) as o:
             for trans_line, o_line in zip(t, o):
-                time_t, x, y, z = [float(x) for x in trans_line.split(",")]
+                time, x, y, z = [float(x) for x in trans_line.split(",")]
                 time_o, roll, pitch, yaw = [float(x) for x in o_line.split(",")]
                 
-                time_t = round(float(time_t), 1) #float(time)
+                time = round(float(time), 1)
 
-                time = time_t
                 if time in times:
                     frame = times[time]
                     if frame in people and frame not in set_frames:
@@ -52,36 +50,43 @@ def set_position(frame_to_data, people):
     for frame, data in frame_to_data.items():
         obj = people[frame][0]
         obj.hide_viewport = True
-        #obj.keyframe_insert(data_path="hide_viewport", frame=1)
+        obj.keyframe_insert(data_path="hide_viewport", frame=1)
 
     
     for frame in range(end+1):
+        if prev_obj:
+            last_frame = int(prev_obj.name.split("o_")[-1])
+            print("Hiding {} at frame {}".format(str(prev_obj), last_frame+1))
+            prev_obj.hide_viewport = True
+            prev_obj.keyframe_insert(data_path="hide_viewport", frame=last_frame+1)
+            bpy.context.scene.frame_set(last_frame+1)
+                
         if frame in frame_to_data:
             # Place mesh for each frame
             obj = people[frame][0]
             data = frame_to_data[frame]
-            
-            # Hide previous object
-            if prev_obj:
-                last_frame = int(prev_obj.name.split("o_")[-1])
-                #print("Hiding {} at frame {}".format(str(prev_obj), last_frame+1))
-                prev_obj.hide_viewport = True
-                #bpy.context.scene.frame_set(frame-1)
-                #prev_obj.keyframe_insert(data_path="hide_viewport", frame=last_frame+1)
-                bpy.context.scene.frame_set(last_frame+1)
                 
             # Set frame location and visibility
             obj.hide_viewport = False
             obj.location = data[0]
             obj.rotation_euler =  data[-1]
             
-            #obj.keyframe_insert(data_path="hide_viewport", frame=frame)
-            #obj.keyframe_insert(data_path="location", frame=frame)
+            obj.keyframe_insert(data_path="hide_viewport", frame=frame)
+            obj.keyframe_insert(data_path="location", frame=frame)
+            print("Showing {} at frame {}".format(obj.name, frame))
             
             prev_obj = obj
+        
+        else:
+            print("Nothing to show at frame ", frame)
             
         bpy.context.scene.frame_set(frame)
-        #print("Showing {} at frame {}".format(obj.name, frame))
+        
+        
+    
+    obj = people[end][0]
+    obj.hide_viewport = True
+    bpy.context.scene.frame_set(end)
        
 
 def get_meshes():
@@ -90,7 +95,6 @@ def get_meshes():
     for obj in collection.all_objects:
         name = obj.name
         
-        # Initalize all  to out of frame
         if name[0] == "o" and "empty" not in name: 
             name = name.split("o_")
             frame = name[-1]
@@ -102,13 +106,9 @@ def get_meshes():
 
         
 def main():
-    '''
-    for a in bpy.data.actions: 
-        a.user_clear()  
-    '''
+
     times = get_timestamps()  
     people = get_meshes()
-    
     positions = get_position(people, times)
     
     print("Number of meshes:", len(people), len(positions))
